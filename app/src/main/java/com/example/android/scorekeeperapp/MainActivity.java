7f4e84ba255d9a74887eb035d757d647861718f0
Steven <vanroyesteven@gmail.com>
2017-02-21 21:53:51 +0100

    Updated version

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.scorekeeperapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,14 @@ public class MainActivity extends AppCompatActivity {
     // fouls of team A and B
     // yellow cards of team A and B
     // red cards of team A and B
-    int scoreA = 0, scoreB=0, foulsA=0, foulsB=0, yellowsA=0, yellowsB=0, redsA=0, redsB=0;
+    int scoreA = 0;
+    int scoreB=0;
+    int foulsA=0;
+    int foulsB=0;
+    int yellowsA=0;
+    int yellowsB=0;
+    int redsA=0;
+    int redsB=0;
     TextView textViewTeamA;
     TextView textViewTeamB;
     TextView textViewFoulsA;
@@ -63,9 +71,7 @@ public class MainActivity extends AppCompatActivity {
         textViewYellowB.setText(""+yellowsB);
         textViewRedA.setText(""+redsA);
         textViewRedB.setText(""+redsB);
-
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,94 +86,67 @@ public class MainActivity extends AppCompatActivity {
         textViewRedA = (TextView) findViewById(R.id.red_a_text_view);
         textViewRedB = (TextView) findViewById(R.id.red_b_text_view);
     }
-
     /*
-    Method to be invoked when the "score" button is pressed (the left score button)
-    i.e. find textview, update score (+1) and set the text to the updated value
+    Method to be invoked when one of the buttons is pressed. The id of the pressed button is used in a switch statement
+    and compared with the ids of all the buttons. Depending on which button is pressed, either :
+    the score is increased
+    the fouls are increased
+    the number of yellow cards is increased
+    the number of red cards is increased
+    everything is reset to 0
      */
-    public void teamAScores(View v){
-        scoreA++;
-        textViewTeamA.setText("" + scoreA);
+    public void buttonClick(View v){
+        switch(v.getId()){
+            case R.id.team_a_scores :
+                scoreA++;
+                textViewTeamA.setText("" + scoreA);
+                break;
+            case R.id.team_b_scores :
+                scoreB++;
+                textViewTeamB.setText("" + scoreB);
+                break;
+            case R.id.plus_fouls_a :
+                foulsA++;
+                textViewFoulsA.setText("" + foulsA);
+                break;
+            case R.id.plus_fouls_b :
+                foulsB++;
+                textViewFoulsB.setText("" + foulsB);
+                break;
+            case R.id.plus_yellow_a :
+                yellowsA++;
+                textViewYellowA.setText("" + yellowsA);
+                break;
+            case R.id.plus_yellow_b :
+                yellowsB++;
+                textViewYellowB.setText("" + yellowsB);
+                break;
+            case R.id.plus_red_a :
+                redsA++;
+                textViewRedA.setText("" + redsA);
+                break;
+            case R.id.plus_red_b :
+                redsB++;
+                textViewRedB.setText("" + redsB);
+                break;
+            case R.id.reset :
+                scoreA=0;
+                scoreB=0;
+                foulsA=0;
+                foulsB=0;
+                yellowsA=0;
+                yellowsB=0;
+                redsA=0;
+                redsB=0;
+                textViewTeamA.setText(""+scoreA);
+                textViewTeamB.setText(""+scoreB);
+                textViewFoulsA.setText(""+foulsA);
+                textViewFoulsB.setText(""+foulsB);
+                textViewYellowA.setText(""+yellowsA);
+                textViewYellowB.setText(""+yellowsB);
+                textViewRedA.setText(""+redsA);
+                textViewRedB.setText(""+redsB);
+                break;
+        }
     }
-
-    /*
-   Method to be invoked when the "score" button is pressed (the right score button)
-   i.e. find textview, update score (+1) and set the text to the updated value
-    */
-    public void teamBScores(View v){
-        scoreB++;
-        textViewTeamB.setText("" + scoreB);
-    }
-
-    /*
-   Method to be invoked when the "+" button of fouls is pressed (the left button)
-   i.e. find textview, update fouls (+1) and set the text to the updated value
-    */
-    public void teamACommitsFoul(View v){
-        foulsA++;
-        textViewFoulsA.setText("" + foulsA);
-    }
-
-    /*
-   Method to be invoked when the "+" button of fouls is pressed (the right button)
-   i.e. find textview, update fouls (+1) and set the text to the updated value
-    */
-    public void teamBCommitsFoul(View v){
-        foulsB++;
-        textViewFoulsB.setText("" + foulsB);
-    }
-
-    /*
-  Method to be invoked when the "+" button of yellow cards is pressed (the left button)
-  i.e. find textview, update yellow cards (+1) and set the text to the updated value
-   */
-    public void teamAYellowCard(View v){
-        yellowsA++;
-        textViewYellowA.setText("" + yellowsA);
-    }
-
-    /*
-  Method to be invoked when the "+" button of yellow cards is pressed (the right button)
-  i.e. find textview, update yellow cards (+1) and set the text to the updated value
-   */
-    public void teamBYellowCard(View v){
-        yellowsB++;
-        textViewYellowB.setText("" + yellowsB);
-    }
-
-    /*
-  Method to be invoked when the "+" button of red cards is pressed (the left button)
-  i.e. find textview, update red cards (+1) and set the text to the updated value
-   */
-    public void teamARedCard(View v){
-        redsA++;
-        textViewRedA.setText("" + redsA);
-    }
-
-    /*
-  Method to be invoked when the "+" button of red cards is pressed (the right button)
-  i.e. find textview, update red cards (+1) and set the text to the updated value
-   */
-    public void teamBRedCard(View v){
-        redsB++;
-        textViewRedB.setText("" + redsB);
-    }
-
-    /*
-    Method to be invoked when the reset button is pressed, i.e. all the values are set to 0
-    and the text on the TextViews is set to this value 0
-     */
-    public void reset(View v){
-        scoreA=0; scoreB=0; foulsA=0;foulsB=0;yellowsA=0;yellowsB=0;redsA=0;redsB=0;
-        textViewTeamA.setText(""+scoreA);
-        textViewTeamB.setText(""+scoreB);
-        textViewFoulsA.setText(""+foulsA);
-        textViewFoulsB.setText(""+foulsB);
-        textViewYellowA.setText(""+yellowsA);
-        textViewYellowB.setText(""+yellowsB);
-        textViewRedA.setText(""+redsA);
-        textViewRedB.setText(""+redsB);
-    }
-
-
 }
